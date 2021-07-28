d82e7b798f07b43badc5bdc50af5c36cedaedea8
Grant Reighard <grant.reighard@gmail.com>
2021-07-27 22:30:45 -0400

    Adds inputs and selects for different editing needs.

--- src/components/singleSite.js
+++ src/components/singleSite.js
@@ -4,6 +4,8 @@ import { useRouteMatch } from 'react-router';
 import { SitesContext } from '../contexts/sitesContext';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './singleSite.css';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
 
 const EDIT_TYPE = {
     ALL: "all",
@@ -15,18 +17,22 @@ const EDIT_TYPE = {
 const SingleSite = () => {
     const { site, fetchSite } = useContext(SitesContext);
     const match = useRouteMatch();
-    const [editedSite, setEditedSite] = useState({});
     const [isEditing, setIsEditing] = useState(false);
-    const [whatIsBeingEdited, setWhatIsBeingEdited] = useState(EDIT_TYPE.ALL)
+    const [whatIsBeingEdited, setWhatIsBeingEdited] = useState(EDIT_TYPE.ALL);
+    const [title, setTitle] = useState("");
+    const [subtitle, setSubtitle] = useState("");
+    const [headerImage, setHeaderImage] = useState("");
+    const [links, setLinks] = useState([]);
 
     useEffect(() => {
         fetchSite(match.params.id)
     }, [])
 
     useEffect(() => {
-        const newSite = Object.assign({}, site);
-        delete newSite._id;
-        setEditedSite(newSite);
+        setTitle(site.title);
+        setSubtitle(site.subtitle);
+        setHeaderImage(site.headerImage);
+        setLinks(site.links);
     }, [site])
 
     const onLinkClick = (linkHref) => {
@@ -62,57 +68,113 @@ const SingleSite = () => {
         fetchSite(match.params.id);
     }
 
+    const transformIconKey = (key) => {
+        const arr = key.split("").slice(2);
+        const display = arr.join("");
+        let valueArr = [];
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i].toUpperCase() === arr[i] && !Number.isInteger(Number(arr[i])) && i !== 0) {
+                valueArr.push("-");
+                valueArr.push(arr[i].toLowerCase());
+            } else if (arr[i].toUpperCase() === arr[i] && !Number.isInteger(Number(arr[i]))) {
+                valueArr.push(arr[i].toLowerCase());
+            } else {
+                valueArr.push(arr[i]);
+            }
+        }
+
+        const value = valueArr.join("");
+        return [display, value];
+    }
+
     const getEditContents = () => {
         switch (whatIsBeingEdited) {
             case "titles":
-                return <div>Titles</div>
+                return <div className="edit-contents">
+                    <h2>Title</h2>
+                    <input type="text" value={title} placeholder="Title" onChange={e => setTitle(e.target.value)} />
+                    <h2>Subtitle</h2>
+                    <input type="text" value={subtitle} placeholder="Subtitle" onChange={e => setSubtitle(e.target.value)} />
+                </div>
             case "images":
-                return <div>Images</div>
+                return <div className="edit-contents">Images</div>
             case "links":
-                return <div>Links</div>
+                return <div className="edit-contents">
+                    <h2>Links</h2>
+                    <ul>
+                        {links.map((link, index) => {
+                            return <>
+                                <input type="text" value={links[index].text} placeholder={`Link #${index + 1}`} onChange={e => {
+                                    const newLinks = links.slice();
+                                    newLinks[index].text = e.target.value;
+                                    setLinks(newLinks);
+                                }} />
+                                <select onChange={e => {
+                                    const newLinks = links.slice();
+                                    newLinks[index].icon = e.target.value;
+                                    setLinks(newLinks);
+                                }}>
+                                    {Object.keys(fab).map(key => {
+                                        const [display, value] = transformIconKey(key);
+                                        return <option value={value}>{display}</option>
+                                    })}
+                                </select>
+                            </>
+                        })}
+                    </ul>
+                </div>
             default: // default and ALL
-                return <div>All</div>
+                return <div className="edit-contents">All</div>
         }
     }
 
-    return (
-        <>
-            {isEditing ? 
-                <div className="edit-tray">
-                    { getEditContents() }
-                </div>
-                :
-                null
-            }
-            <div className="single-site-container">
-                <div className="edit-and-save-buttons">
+    const getDisplayContents = (title, subtitle, headerImage, links) => {
+        return <div className="single-site-container">
+                <div className="edit-button">
                     { isEditing ? 
-                        <div className="save-and-cancel-buttons">
-                            <FontAwesomeIcon icon={["far", "save"]} size="3x" onClick={onSave} />
-                            <FontAwesomeIcon icon={["far", "window-close"]} size="3x" onClick={onCancel} />
-                        </div>
+                        null
                         : 
                         <FontAwesomeIcon icon={["far", "edit"]} size="3x" onClick={() => setIsEditing(true)} />
                     }
                 </div>
-                <h1 className="single-title" onClick={() => setWhatIsBeingEdited(EDIT_TYPE.TITLES)}>{site?.title}</h1>
-                <p className="single-subtitle" onClick={() => setWhatIsBeingEdited(EDIT_TYPE.TITLES)}>{site?.subtitle}</p>
+                <h1 className="single-title" onClick={() => setWhatIsBeingEdited(EDIT_TYPE.TITLES)}>{title}</h1>
+                <p className="single-subtitle" onClick={() => setWhatIsBeingEdited(EDIT_TYPE.TITLES)}>{subtitle}</p>
                 <img 
-                    src={site.headerImage || "https://via.placeholder.com/300x300?text=image+here"} 
-                    alt={site.title} className="header-image"
+                    src={headerImage || "https://via.placeholder.com/300x300?text=image+here"} 
+                    alt={title} className="header-image"
                     onClick={() => setWhatIsBeingEdited(EDIT_TYPE.IMAGES)}
                 />
-                {site?.links ?
+                {links ?
                     <ul className="links-list">
-                        {site.links.map((link) => {
+                        {links.map((link) => {
                             return <li onClick={() => onLinkClick(link.href)} className="individual-link">
-                                <div className="link-text">{link.text} {link?.hits}</div>
-                                <FontAwesomeIcon icon={["fab", "github"]} />
+                                <div className="link-text">{link.text}</div>
+                                <FontAwesomeIcon icon={["fab", link.icon]} />
                             </li>
                         })}
                     </ul>
                 : null}
             </div>
+    }
+
+    return (
+        <>
+            { isEditing ? 
+                <div className="edit-tray">
+                    <div className="save-and-cancel-buttons">
+                        <FontAwesomeIcon icon={["far", "save"]} size="3x" onClick={onSave} color="lightgreen" />
+                        <FontAwesomeIcon icon={["far", "window-close"]} size="3x" onClick={onCancel} color="red" />
+                    </div>
+                    { getEditContents() }
+                </div>
+                :
+                null
+            }
+            { isEditing ?
+                getDisplayContents(title, subtitle, headerImage, links)
+                :
+                getDisplayContents(site.title, site.subtitle, site.headerImage, site.links)
+            }
         </>
     )
 }
