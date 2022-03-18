857381643dde59e4fbeaf124dc33f206728e19ee
Grant Reighard <grant.reighard@gmail.com>
2022-03-18 18:01:15 -0400

    Uses a water video as the landing background.

--- src/components/landing.js
+++ src/components/landing.js
@@ -2,6 +2,7 @@ import React, { useEffect, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { SitesContext } from '../contexts/sitesContext';
+import waterMovie from '../components/assets/water.m4v';
 
 import './landing.css';
 
@@ -14,7 +15,11 @@ const Landing = () => {
 
     return (
         <div className="landing-container">
-            <div className="landing-behind"></div>
+            <div className="landing-behind">
+                <video autoPlay muted loop id="myVideo" width="500" height="500">
+                    <source src={waterMovie} type="video/mp4" />
+                </video>
+            </div>
              <div className="landing" style={{ backgroundColor: themeObj.landingCardBackground }}>
                 <Link to="/account/login" style={{ color: themeObj.color }}><FontAwesomeIcon icon={["fas", "plus-square"]} size="3x"/></Link>
                 <h1><Link to="/account/login" style={{ color: themeObj.color }}>Log in to start creating.</Link></h1>
