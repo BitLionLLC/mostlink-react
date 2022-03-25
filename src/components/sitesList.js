faf6b53abd8adfcba8ab766ac74a3348e574fb2c
Grant Reighard <grant.reighard@gmail.com>
2022-03-25 18:20:33 -0400

    Separates the ul from the else of the ternary.

--- src/components/sitesList.js
+++ src/components/sitesList.js
@@ -12,9 +12,9 @@ const SitesList = () => {
 
     return (
         <div className="sites-container">
-            <ul className="sites-list">
-                {sites.length ? 
-                    sites.map((site, i) => {
+            {sites.length ? 
+                <ul className="sites-list">
+                    {sites.map((site, i) => {
                         return <Link to={`/site/${site._id}`} className="site-link">
                             <li key={site._id} className="site-box" style={{backgroundColor: `rgba(0,${20*i+100},0)`}}>
                                 <h2 className="title" style={{ color: themeObj.color }}>{site.title}</h2>
@@ -22,18 +22,18 @@ const SitesList = () => {
                                 <img src={site.screenshot || "https://via.placeholder.com/375x280?text=screenshot+here"} width="375" height="280" className="site-screenshot"></img>
                             </li>
                         </Link>
-                })
-                :
-                    <div className="empty-sites-container">
-                        <div className="empty-sites" style={{backgroundColor: themeObj.landingCardBackground}}>
-                            <h1>Create a site with the
-                                <div className="create-site-clone" style={{color: themeObj.accentColor, backgroundColor: themeObj.bodyColor}}>+</div> 
-                                button to get started!
-                            </h1>
-                        </div>
+                    })}
+                </ul>
+            :
+                <div className="empty-sites-container">
+                    <div className="empty-sites" style={{backgroundColor: themeObj.landingCardBackground}}>
+                        <h1>Create a site with the
+                            <div className="create-site-clone" style={{color: themeObj.accentColor, backgroundColor: themeObj.bodyColor}}>+</div> 
+                            button to get started!
+                        </h1>
                     </div>
-                }
-            </ul>
+                </div>
+            }
         </div>
     )
 }
