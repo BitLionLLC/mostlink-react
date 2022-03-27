f7279b25a1cece7f2b8adf80e290ea0ec203c4b6
Grant Reighard <grant.reighard@gmail.com>
2022-03-26 20:08:26 -0400

    Adds default screenshot for sitesList.

--- src/components/sitesList.js
+++ src/components/sitesList.js
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect } from 'react';
 import { SitesContext } from '../contexts/sitesContext';
 import { Link } from 'react-router-dom';
+import defaultScreenshot from './assets/default-screenshot.png';
 import './sitesList.css';
 
 const SitesList = () => {
@@ -19,7 +20,7 @@ const SitesList = () => {
                             <li key={site._id} className="site-box" style={{backgroundColor: `rgba(0,${20*i+100},0)`}}>
                                 <h2 className="title" style={{ color: themeObj.color }}>{site.title}</h2>
                                 <p className="subtitle" style={{ color: themeObj.color }}>{site.subtitle || "subtitle"}</p>
-                                <img src={site.screenshot || "https://via.placeholder.com/375x280?text=screenshot+here"} width="375" height="280" className="site-screenshot"></img>
+                                <img src={site.screenshot || defaultScreenshot} width="375" height="280" className="site-screenshot"></img>
                             </li>
                         </Link>
                     })}
