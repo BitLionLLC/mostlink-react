a7f8fa4756ad898128a7b36ac3d3322c6223755a
Grant Reighard <grant.reighard@gmail.com>
2022-04-18 19:53:23 -0400

    Makes use of Stripe portal session endpoint.

--- src/components/payment/paymentSuccess.js
+++ src/components/payment/paymentSuccess.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import axios from 'axios';
 
 const PaymentSuccess = () => {
+    const createPortalSession = () => {
+        axios
+            .get('/payment/create-portal-session')
+            .then((res) => {
+                window.location.href = res.data.redirect;
+            })
+            .catch(err => console.log(err))
+    }
+
     return (
         <div>
-            Success
+            Payment succeeded!
+            <button onClick={createPortalSession}>Log into Stripe portal</button>
         </div>
     )
 }
