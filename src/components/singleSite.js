0e325348905798c74e6ac0cc77187b142855c95b
Grant Reighard <grant.reighard@gmail.com>
2022-06-18 22:53:11 -0400

    Disables animation while editing.

--- src/components/singleSite.js
+++ src/components/singleSite.js
@@ -688,7 +688,7 @@ const SingleSite = () => {
         document.body.style.backgroundImage = thisBodyGradient;
         
         return <div className={styles.singleSiteWrapper} style={{ justifyContent: isEditing ? 'flex-end' : 'center', paddingRight: isEditing ? '50px': 0 }}>
-            {thisBodyAnimationStyle && <Particles id="tsparticles" init={particlesInit} loaded={particlesLoaded} options={{...ANIMATION_PRESETS[thisBodyAnimationStyle], autoplay: true}} style={{height: '100vh', width: '100vw'}} />}
+            {thisBodyAnimationStyle && !isEditing && <Particles id="tsparticles" init={particlesInit} loaded={particlesLoaded} options={{...ANIMATION_PRESETS[thisBodyAnimationStyle], autoplay: true}} style={{height: '100vh', width: '100vw'}} />}
              <style children={isEditing ? keyFramesStartEdit : keyFramesEndEdit} />
              <div className={styles.singleSiteContainer} style={{ backgroundColor: thisContainerColor, backgroundImage: thisContainerGradient, ...singleSiteStyle, animationName: hasEditButtonBeenClicked && (isEditing ? 'single-site-move-right' : 'single-site-move-left'), animationDuration: '2s' }}>
                 <Prompt when={isDirty} message='Reload site? Changes you made may not be saved.' />
