491731d98aec4d7faa5963a7547b86ff153ecfe4
Grant Reighard <grant.reighard@gmail.com>
2022-07-01 16:15:10 -0400

    Fixes dollar sign bug in subdomain URIs.

--- src/components/singleSite.js
+++ src/components/singleSite.js
@@ -621,11 +621,11 @@ const SingleSite = () => {
                                 <>
                                     <a href={`https://www.${process.env.REACT_APP_HOSTED_BASE}/${site.subdomain}`} style={{color: themeObj.color}} target="_blank" rel="noreferrer">https://www.{process.env.REACT_APP_HOSTED_BASE}/{site.subdomain}</a>
                                     <br/>
-                                    <a href={`https://${site.subdomain}.${process.env.REACT_APP_HOSTED_BASE}`} style={{color: themeObj.color}} target="_blank" rel="noreferrer">https://${site.subdomain}.{process.env.REACT_APP_HOSTED_BASE}</a>
+                                    <a href={`https://${site.subdomain}.${process.env.REACT_APP_HOSTED_BASE}`} style={{color: themeObj.color}} target="_blank" rel="noreferrer">https://{site.subdomain}.{process.env.REACT_APP_HOSTED_BASE}</a>
                                     <br/>
                                     <a href={`https://www.${process.env.REACT_APP_HOSTED_BASE_SHORT}/${site.subdomain}`} style={{color: themeObj.color}} target="_blank" rel="noreferrer">https://www.{process.env.REACT_APP_HOSTED_BASE_SHORT}/{site.subdomain}</a>
                                     <br/>
-                                    <a href={`https://${site.subdomain}.${process.env.REACT_APP_HOSTED_BASE_SHORT}`} style={{color: themeObj.color}} target="_blank" rel="noreferrer">https://${site.subdomain}.{process.env.REACT_APP_HOSTED_BASE_SHORT}</a>
+                                    <a href={`https://${site.subdomain}.${process.env.REACT_APP_HOSTED_BASE_SHORT}`} style={{color: themeObj.color}} target="_blank" rel="noreferrer">https://{site.subdomain}.{process.env.REACT_APP_HOSTED_BASE_SHORT}</a>
                                 </>
                             :
                                 <>
