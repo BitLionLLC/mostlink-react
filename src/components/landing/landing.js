9f24c691ac349746cfac37ceb5d724beeeb3b5ab
Grant Reighard <grant.reighard@gmail.com>
2023-05-23 20:56:11 -0400

    More adjustments to landing.

--- src/components/landing/landing.js
+++ src/components/landing/landing.js
@@ -19,17 +19,20 @@ const Landing = () => {
   return (
     <div className={styles.landingContainer}>
       <div className={styles.titlesContainer}>
-        <h1>A modern link page builder for content creators and business owners</h1>
-        <h2>Build your own custom site today with our easy-to-use builder. Start for free!</h2>
-      </div>
-
-      <div className={styles.landingInner}>
-        <div className={styles.landingBehind}>
-          <img src={waterGif} alt="flowing water" />
+        <div className={styles.heroTitle}>
+          <h1>A modern<br />link in bio</h1>
         </div>
-        <div className={styles.landing} style={{ backgroundColor: themeObj.landingCardBackground }}>
-          <Link to="/account/login" style={{ color: themeObj.color }}><FontAwesomeIcon icon={['fas', 'plus-square']} size="3x"/></Link>
-          <h1><Link to="/account/login" style={{ color: themeObj.color }}>Log in to start creating custom link pages.</Link></h1>
+
+        <div className={styles.landingInnerWrapper}>
+          <div className={styles.landingInner}>
+            <div className={styles.landingBehind}>
+              <img src={waterGif} alt="flowing water" />
+            </div>
+            <div className={styles.landing} style={{ backgroundColor: themeObj.landingCardBackground }}>
+              <Link to="/account/register" style={{ color: themeObj.color }}><FontAwesomeIcon icon={['fas', 'plus-square']} size="3x" /></Link>
+              <h1><Link to="/account/register" style={{ color: themeObj.color }}>Register to start creating custom link pages.</Link></h1>
+            </div>
+          </div>
         </div>
       </div>
 
@@ -37,7 +40,7 @@ const Landing = () => {
         <Features />
         <Examples />
         <FAQs />
-        <iframe src="https://cdn.forms-content.sg-form.com/a3165ac8-03da-11ed-90c9-7edf428d4714" title="newsletter-signup" className={styles.newsletterSignup}/>
+        <iframe src="https://cdn.forms-content.sg-form.com/a3165ac8-03da-11ed-90c9-7edf428d4714" title="newsletter-signup" className={styles.newsletterSignup} />
       </div>
     </div>
   );
