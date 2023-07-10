5106c785439d7524ee59c133891c9266b387842a
Grant Reighard <grant.reighard@gmail.com>
2023-07-10 13:16:14 -0400

    Changes mostlink.io form

--- src/components/singleSite.js
+++ src/components/singleSite.js
@@ -686,7 +686,7 @@ const SingleSite = () => {
               process.env.REACT_APP_ENVIRONMENT === 'production' || process.env.REACT_APP_ENVIRONMENT === 'development'
                 ?
                 <>
-                  <a href={`https://${site.subdomain}.${process.env.REACT_APP_HOSTED_BASE}`} style={{color: themeObj.color}} target="_blank" rel="noreferrer">https://{site.subdomain}.{process.env.REACT_APP_HOSTED_BASE}</a>
+                  <a href={`https://www.${process.env.REACT_APP_HOSTED_BASE}/${site.subdomain}`} style={{color: themeObj.color}} target="_blank" rel="noreferrer">https://www.{process.env.REACT_APP_HOSTED_BASE}/${site.subdomain}</a>
                   <br/>
                   <a href={`https://www.${process.env.REACT_APP_HOSTED_BASE_SHORT}/${site.subdomain}`} style={{color: themeObj.color}} target="_blank" rel="noreferrer">https://www.{process.env.REACT_APP_HOSTED_BASE_SHORT}/{site.subdomain}</a>
                 </>
