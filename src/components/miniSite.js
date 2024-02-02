03b4920fac52e88f3a273c18096eef8d99d043a3
Grant Reighard <grant.reighard@gmail.com>
2024-02-02 18:14:07 -0500

    Change edit icon color

--- src/components/miniSite.js
+++ src/components/miniSite.js
@@ -5,8 +5,9 @@ import defaultHeader from "./assets/default-header.png";
 
 import styles from "./miniSite.module.css";
 
-const MiniSite = ({site}) => {
-  const { setIsEditModalOpen, setEditModalOpenedWith } = useContext(SitesContext);
+const MiniSite = ({ site }) => {
+  const { setIsEditModalOpen, setEditModalOpenedWith } =
+    useContext(SitesContext);
 
   const {
     title,
@@ -21,34 +22,82 @@ const MiniSite = ({site}) => {
     containerGradient,
     liveNotificationColor,
     subdomain,
-    _id
+    _id,
   } = site;
 
   return (
-    <div className={styles.miniSiteWrapper} style={{backgroundColor: containerColor, backgroundImage: containerGradient}}>
-      <FontAwesomeIcon icon={["far", "edit"]} size="3x" className={styles.editButton} onClick={(e) => {
-        e.preventDefault();
-        setIsEditModalOpen(true);
-        setEditModalOpenedWith({ title, subtitle, subdomain, id: _id });
-      }} />
-      {
-        headerEmoji 
-          ?
-          <span className={styles.headerEmoji}>{headerEmoji}</span>
-          :
-          <img src={headerImage?.url || headerImage?.base64 || defaultHeader} alt='header' width="75" height="75" className={styles.headerImage} />
-      }
-      <span className={styles.title} style={{color: titlesColor}}>{title}</span>
-      <span className={styles.subtitle} style={{color: titlesColor}}>{subtitle}</span>
+    <div
+      className={styles.miniSiteWrapper}
+      style={{
+        backgroundColor: containerColor,
+        backgroundImage: containerGradient,
+      }}
+    >
+      <FontAwesomeIcon
+        icon={["far", "edit"]}
+        size="3x"
+        className={styles.editButton}
+        color="black"
+        onClick={(e) => {
+          e.preventDefault();
+          setIsEditModalOpen(true);
+          setEditModalOpenedWith({ title, subtitle, subdomain, id: _id });
+        }}
+      />
+      {headerEmoji ? (
+        <span className={styles.headerEmoji}>{headerEmoji}</span>
+      ) : (
+        <img
+          src={headerImage?.url || headerImage?.base64 || defaultHeader}
+          alt="header"
+          width="75"
+          height="75"
+          className={styles.headerImage}
+        />
+      )}
+      <span className={styles.title} style={{ color: titlesColor }}>
+        {title}
+      </span>
+      <span className={styles.subtitle} style={{ color: titlesColor }}>
+        {subtitle}
+      </span>
       <ul className={styles.linksList}>
         {links.map((link, i) => {
-          return <li className={styles.linkRow} key={i} style={{backgroundColor: linkBackgroundColor}}>
-            <div className={styles.linkAndLive}>
-              <span style={{color: linkTextColor}} className={styles.linkText}>{link.text}&nbsp;</span>
-              {link.live ? <div>{link.live.isLive ? <><span>-</span><span style={{color: liveNotificationColor}}> LIVE!</span></> : "- not live"}</div> : null}
-            </div>   
-            <FontAwesomeIcon icon={link?.icon?.split("_")} style={{color: linkTextColor}} />
-          </li>;
+          return (
+            <li
+              className={styles.linkRow}
+              key={i}
+              style={{ backgroundColor: linkBackgroundColor }}
+            >
+              <div className={styles.linkAndLive}>
+                <span
+                  style={{ color: linkTextColor }}
+                  className={styles.linkText}
+                >
+                  {link.text}&nbsp;
+                </span>
+                {link.live ? (
+                  <div>
+                    {link.live.isLive ? (
+                      <>
+                        <span>-</span>
+                        <span style={{ color: liveNotificationColor }}>
+                          {" "}
+                          LIVE!
+                        </span>
+                      </>
+                    ) : (
+                      "- not live"
+                    )}
+                  </div>
+                ) : null}
+              </div>
+              <FontAwesomeIcon
+                icon={link?.icon?.split("_")}
+                style={{ color: linkTextColor }}
+              />
+            </li>
+          );
         })}
       </ul>
     </div>
