b3b1f98eec87ac8e33f73305977c17daddba5406
Grant Reighard <grant.reighard@gmail.com>
2024-02-08 20:43:03 -0500

    Fixes padding on laptop

--- src/components/landing/landing.js
+++ src/components/landing/landing.js
@@ -24,16 +24,32 @@ const Landing = () => {
     <div className={styles.landingContainer}>
       <div className={styles.titlesContainer}>
         <div className={styles.heroTitle}>
-          <h1>A modern<br />link in bio</h1>
+          <h1>
+            A modern
+            <br />
+            link in bio
+          </h1>
         </div>
 
         <div className={styles.landingInner}>
           <div className={styles.landingBelow}>
-            <img src={reducedMotion ? waterStill : waterGif} alt="flowing water" />
+            <img
+              src={reducedMotion ? waterStill : waterGif}
+              alt="flowing water"
+            />
           </div>
-          <div className={styles.landingAbove} style={{ backgroundColor: themeObj.landingCardBackground }}>
-            <Link to="/account/register" style={{ color: themeObj.color }}><FontAwesomeIcon icon={["fas", "plus-square"]} size="3x" /></Link>
-            <h1><Link to="/account/register" style={{ color: themeObj.color }}>Register to start creating custom link pages.</Link></h1>
+          <div
+            className={styles.landingAbove}
+            style={{ backgroundColor: themeObj.landingCardBackground }}
+          >
+            <Link to="/account/register" style={{ color: themeObj.color }}>
+              <FontAwesomeIcon icon={["fas", "plus-square"]} size="3x" />
+            </Link>
+            <h1>
+              <Link to="/account/register" style={{ color: themeObj.color }}>
+                Register to start creating custom link pages.
+              </Link>
+            </h1>
           </div>
         </div>
       </div>
@@ -42,7 +58,11 @@ const Landing = () => {
         <Features />
         <Examples />
         <FAQs />
-        <iframe src="https://cdn.forms-content.sg-form.com/a3165ac8-03da-11ed-90c9-7edf428d4714" title="newsletter-signup" className={styles.newsletterSignup} />
+        <iframe
+          src="https://cdn.forms-content.sg-form.com/a3165ac8-03da-11ed-90c9-7edf428d4714"
+          title="newsletter-signup"
+          className={styles.newsletterSignup}
+        />
       </div>
     </div>
   );
